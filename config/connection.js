db0244a7f7a6ad6e8f9484644523e924641059d7
Emilee  Reiter <emileereiter@Emilees-MacBook-Air.local>
2020-07-23 20:02:18 -0600

    included jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ const mysql = require ("mysql")
 const util = require ("util")
 var connection; 
 
-connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'notetaker_db'
-})
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'notetaker_db'
+    })
+}
 
 connection.connect(function(err){
     if(err){
